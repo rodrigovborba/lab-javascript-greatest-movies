5e33e7d55e608adaed4086022560fa7fc70fea7c
Rodrigo Borba <rodrigoborba@MacBook-Pro-de-Rodrigo.local>
2019-10-24 15:54:23 +0100

    missing-5-iterations

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -1,8 +1,26 @@
-/* eslint no-restricted-globals: 'off' */
+  /* eslint no-restricted-globals: 'off' */
 
 // Iteration 1: All rates average - Get the average of all rates with 2 decimals
 
+function calculateAverageMovieRate (arrayMovies) {
+    let sum = 0;
+    for (let i = 0; i < arrayMovies.length; i++) {
+        sum += arrayMovies[i].rate;
+    }
+    return sum/arrayMovies.length;
+}
 // Iteration 2: Drama movies - Get the average of Drama Movies
+
+function calculateAverageDramaRate (arrayMoviesDrama) {
+    let moviesDramaFiltered = arrayMoviesDrama.filter((movie) => {
+        if (movie.genre.includes('Drama')) {
+            return true;
+        } else {
+            return false;
+        }
+    })
+        return calculateAverageMovieRate(moviesDramaFiltered);
+}
 
 // Iteration 3: Ordering by duration - Order by time duration, ascending (in growing order)
 
